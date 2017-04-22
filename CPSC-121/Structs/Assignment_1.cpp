2cf553d844396824f89d775bf5be5be6a0f30f59
royredman4 <royredman4@gmail.com>
2017-04-22 11:40:48 -0700

    Solves the First assignment

---
+++ CPSC-121/Structs/Assignment_1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Professor{
+	string name;
+	string course_name;
+	string textbook;
+};
+
+struct Student{
+	string name;
+	int age;
+	double weight;
+	string couses[10];
+	int course_size;
+	bool is_female;
+	Professor best_professor;
+
+};
+
+int main() {
+
+return 0;
+}
